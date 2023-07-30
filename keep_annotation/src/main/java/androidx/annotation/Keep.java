2d5dcb909f19fd7a91ea49bcba9d3cfa0a6016ca
v7878 <46866705+vova7878@users.noreply.github.com>
2023-07-30 22:36:38 +0700

    add Reflection

---
+++ keep_annotation/src/main/java/androidx/annotation/Keep.java
@@ -0,0 +1,17 @@
+package androidx.annotation;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PACKAGE;
+import static java.lang.annotation.ElementType.TYPE;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target({PACKAGE, TYPE, ANNOTATION_TYPE, CONSTRUCTOR, METHOD, FIELD})
+public @interface Keep {
+}
