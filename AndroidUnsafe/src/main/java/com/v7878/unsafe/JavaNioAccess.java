1fc11678cd129e7da45b077e5664d212695f107c
v7878 <46866705+vova7878@users.noreply.github.com>
2023-08-13 19:02:04 +0700

    AndroidUnsafe: format code

--- AndroidUnsafe/src/main/java/com/v7878/unsafe/JavaNioAccess.java
+++ AndroidUnsafe/src/main/java/com/v7878/unsafe/JavaNioAccess.java
@@ -24,7 +24,7 @@ import java.nio.ByteBuffer;
 
 import dalvik.system.DexFile;
 
-// SegmentByteBuffer should not appear in JavaNioAccess as it breaks class loading sequence
+// SegmentByteBuffer should not appear in JavaNioAccess as it breaks class loading order
 class BufferFactory {
     public static ByteBuffer makeBuf(long address, int cap) {
         return new SegmentByteBuffer(new DirectByteBuffer.MemoryRef(address), -1, 0, cap, cap, 0, false);
@@ -53,12 +53,16 @@ public class JavaNioAccess {
         //}
         mem_def.getClassData().getDirectMethods().add(new EncodedMethod(
                 MethodId.constructor(mem_ref_id, TypeId.J),
-                Modifier.PUBLIC | /*TODO: CONSTRUCTOR*/ 0x10000).withCode(CORRECT_SDK_INT == 26 ? 0 : 1, b -> b
+                Modifier.PUBLIC | /*TODO: CONSTRUCTOR*/ 0x10000
+        ).withCode(CORRECT_SDK_INT == 26 ? 0 : 1, b -> b
                 .if_(CORRECT_SDK_INT == 26,
-                        unused -> b.invoke(DIRECT, MethodId.constructor(nio_mem_ref_id, TypeId.J),
-                                b.this_(), b.p(0), b.p(1)),
-                        unused -> b.const_4(b.l(0), 0)
-                                .invoke(DIRECT, MethodId.constructor(nio_mem_ref_id, TypeId.J, TypeId.of(Object.class)),
+                        unused -> b
+                                .invoke(DIRECT, MethodId.constructor(nio_mem_ref_id, TypeId.J),
+                                        b.this_(), b.p(0), b.p(1)),
+                        unused -> b
+                                .const_4(b.l(0), 0)
+                                .invoke(DIRECT, MethodId.constructor(nio_mem_ref_id,
+                                                TypeId.J, TypeId.of(Object.class)),
                                         b.this_(), b.p(0), b.p(1), b.l(0)))
                 .return_void()
         ));
@@ -73,7 +77,8 @@ public class JavaNioAccess {
         buf_def.getClassData().getDirectMethods().add(new EncodedMethod(
                 MethodId.constructor(direct_buf_id, mem_ref_id,
                         TypeId.I, TypeId.I, TypeId.I, TypeId.I, TypeId.I, TypeId.Z),
-                Modifier.PUBLIC | /*TODO: CONSTRUCTOR*/ 0x10000).withCode(0, b -> b
+                Modifier.PUBLIC | /*TODO: CONSTRUCTOR*/ 0x10000
+        ).withCode(0, b -> b
                 .invoke_range(DIRECT, MethodId.constructor(nio_direct_buf_id, nio_mem_ref_id,
                                 TypeId.I, TypeId.I, TypeId.I, TypeId.I, TypeId.I, TypeId.Z),
                         8, b.this_())
