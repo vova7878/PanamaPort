9138bc7f1fc4e7bbf4b926d715a02aa1f1430eec
v7878 <46866705+vova7878@users.noreply.github.com>
2024-02-10 00:17:26 +0700

    AndroidUnsafe: fix LibArt

--- AndroidUnsafe/src/main/java/com/v7878/unsafe/foreign/LibArt.java
+++ AndroidUnsafe/src/main/java/com/v7878/unsafe/foreign/LibArt.java
@@ -1,9 +1,10 @@
 package com.v7878.unsafe.foreign;
 
-import com.v7878.foreign.Linker;
+import com.v7878.foreign.Arena;
 import com.v7878.foreign.SymbolLookup;
 
 public class LibArt {
     //TODO: open libart.so with art namespace via android_dlopen_ext
-    public static final SymbolLookup ART = Linker.nativeLinker().defaultLookup();
+    public static final SymbolLookup ART = SymbolLookup
+            .libraryLookup("libart.so", Arena.global());
 }
