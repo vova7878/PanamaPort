fa2ec2038225d6a939cca6bbef2a5a547060ceb4
v7878 <46866705+vova7878@users.noreply.github.com>
2024-02-17 22:12:58 +0700

    LLVM: temporary fix LibLLVM for android 8+

--- LLVM/src/main/java/com/v7878/llvm/LibLLVM.java
+++ LLVM/src/main/java/com/v7878/llvm/LibLLVM.java
@@ -1,6 +1,7 @@
 package com.v7878.llvm;
 
 import static com.v7878.misc.Version.CORRECT_SDK_INT;
+import static com.v7878.unsafe.AndroidUnsafe.IS64BIT;
 
 import com.v7878.foreign.Arena;
 import com.v7878.foreign.SymbolLookup;
@@ -8,6 +9,8 @@ import com.v7878.foreign.SymbolLookup;
 public class LibLLVM {
     //TODO: heap session
     public static final Arena LLVM_SCOPE = Arena.global();
+    //TODO: open with system namespace via android_dlopen_ext
     public static final SymbolLookup LLVM = SymbolLookup.libraryLookup(
-            CORRECT_SDK_INT < 28 ? "libLLVM.so" : "libLLVM_android.so", LLVM_SCOPE);
+            CORRECT_SDK_INT < 28 ? "libLLVM.so" :
+                    "/system/lib" + (IS64BIT ? "64" : "") + "/libLLVM_android.so", LLVM_SCOPE);
 }
