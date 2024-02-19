d7541dd6f05dd45fd64c7a3247ae15972c54cab0
v7878 <46866705+vova7878@users.noreply.github.com>
2024-02-19 21:08:31 +0700

    AndroidUnsafe: add LibArt.ART_SCOPE

--- AndroidUnsafe/src/main/java/com/v7878/unsafe/foreign/LibArt.java
+++ AndroidUnsafe/src/main/java/com/v7878/unsafe/foreign/LibArt.java
@@ -4,7 +4,9 @@ import com.v7878.foreign.Arena;
 import com.v7878.foreign.SymbolLookup;
 
 public class LibArt {
+    //TODO: heap session
+    public static final Arena ART_SCOPE = Arena.global();
     //TODO: open libart.so with art namespace via android_dlopen_ext
     public static final SymbolLookup ART = SymbolLookup
-            .libraryLookup("libart.so", Arena.global());
+            .libraryLookup("libart.so", ART_SCOPE);
 }
