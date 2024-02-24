224d0296ae942300090e99961d13f30e8c49aa71
v7878 <46866705+vova7878@users.noreply.github.com>
2024-02-24 15:41:39 +0700

    LLVM: add Analysis

---
+++ LLVM/src/main/java/com/v7878/llvm/Analysis.java
@@ -0,0 +1,192 @@
+package com.v7878.llvm;
+
+import static com.v7878.foreign.ValueLayout.ADDRESS;
+import static com.v7878.llvm.LibLLVM.LLVM;
+import static com.v7878.llvm.LibLLVM.LLVM_SCOPE;
+import static com.v7878.llvm.Types.LLVMBool;
+import static com.v7878.llvm.Types.cLLVMModuleRef;
+import static com.v7878.llvm.Types.cLLVMValueRef;
+import static com.v7878.llvm._Utils.CHAR_PTR;
+import static com.v7878.llvm._Utils.ENUM;
+import static com.v7878.llvm._Utils.addressToLLVMString;
+import static com.v7878.llvm._Utils.ptr;
+import static com.v7878.unsafe.Utils.nothrows_run;
+
+import com.v7878.foreign.Arena;
+import com.v7878.foreign.MemorySegment;
+import com.v7878.llvm.Types.LLVMModuleRef;
+import com.v7878.llvm.Types.LLVMValueRef;
+import com.v7878.unsafe.foreign.SimpleBulkLinker;
+import com.v7878.unsafe.foreign.SimpleBulkLinker.SymbolHolder2;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodType;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.LongSupplier;
+import java.util.function.Supplier;
+
+public class Analysis {
+
+    /*
+     * @defgroup LLVMCAnalysis Analysis
+     * @ingroup LLVMC
+     */
+
+    static final Class<?> cLLVMVerifierFailureAction = ENUM;
+
+    public enum LLVMVerifierFailureAction {
+
+        /**
+         * verifier will print to stderr and abort()
+         */
+        LLVMAbortProcessAction,
+        /**
+         * verifier will print to stderr and return 1
+         */
+        LLVMPrintMessageAction,
+        /**
+         * verifier will just return 1
+         */
+        LLVMReturnStatusAction;
+
+        public int value() {
+            return ordinal();
+        }
+
+        public static LLVMVerifierFailureAction of(int value) {
+            for (var e : values()) {
+                if (e.value() == value) {
+                    return e;
+                }
+            }
+            throw new IllegalArgumentException("value: " + value + " is not found");
+        }
+    }
+
+    private enum Function implements SymbolHolder2 {
+        LLVMVerifyModule(LLVMBool, cLLVMModuleRef, cLLVMVerifierFailureAction, ptr(CHAR_PTR)),
+        LLVMVerifyFunction(LLVMBool, cLLVMValueRef, cLLVMVerifierFailureAction),
+        LLVMViewFunctionCFG(void.class, cLLVMValueRef),
+        LLVMViewFunctionCFGOnly(void.class, cLLVMValueRef);
+
+        static {
+            SimpleBulkLinker.processSymbols(LLVM, LLVM_SCOPE, Function.values());
+        }
+
+        private final MethodType type;
+
+        private LongSupplier symbol;
+        private Supplier<MethodHandle> handle;
+
+        Function(Class<?> rtype, Class<?>... atypes) {
+            this.type = MethodType.methodType(rtype, atypes);
+        }
+
+        @Override
+        public MethodType type() {
+            return type;
+        }
+
+        @Override
+        public void setSymbol(LongSupplier symbol) {
+            this.symbol = symbol;
+        }
+
+        @Override
+        public void setHandle(Supplier<MethodHandle> handle) {
+            this.handle = handle;
+        }
+
+        public long symbol() {
+            return symbol.getAsLong();
+        }
+
+        public MethodHandle handle() {
+            return Objects.requireNonNull(handle.get());
+        }
+
+        @Override
+        public String toString() {
+            return name() + "{" +
+                    "type=" + type +
+                    ", symbol=" + symbol() +
+                    ", handle=" + handle() + '}';
+        }
+    }
+
+
+    /**
+     * Verifies that a module is valid, taking the specified action if not.
+     * Optionally returns a human-readable description of any invalid constructs.
+     */
+    public static boolean LLVMVerifyModule(LLVMModuleRef M, LLVMVerifierFailureAction Action, Consumer<String> OutMessage) {
+        try (Arena arena = Arena.ofConfined()) {
+            MemorySegment c_OutMessage = arena.allocate(ADDRESS);
+            boolean err = nothrows_run(() -> (boolean) Function.LLVMVerifyModule.handle()
+                    .invoke(M.value(), Action.value(), c_OutMessage.nativeAddress()));
+            if (err) {
+                OutMessage.accept(addressToLLVMString(c_OutMessage.get(ADDRESS, 0).nativeAddress()));
+            }
+            return err;
+        }
+    }
+
+    /**
+     * Verifies that a module is valid, taking the specified action if not.
+     */
+    // Port-added
+    public static void LLVMVerifyModule(LLVMModuleRef M, LLVMVerifierFailureAction Action) throws LLVMException {
+        String[] err = new String[1];
+        if (LLVMVerifyModule(M, Action, E -> err[0] = E)) {
+            throw new LLVMException(err[0]);
+        }
+    }
+
+    /**
+     * Verifies that a module is valid.
+     */
+    // Port-added
+    public static void LLVMVerifyModule(LLVMModuleRef M) throws LLVMException {
+        LLVMVerifyModule(M, LLVMVerifierFailureAction.LLVMReturnStatusAction);
+    }
+
+    /**
+     * Verifies that a single function is valid, taking the specified action. Useful
+     * for debugging.
+     */
+    public static boolean nLLVMVerifyFunction(LLVMValueRef Fn, LLVMVerifierFailureAction Action) {
+        return nothrows_run(() -> (boolean) Function.LLVMViewFunctionCFGOnly.handle().invoke(Fn.value(), Action.value()));
+    }
+
+    /**
+     * Verifies that a single function is valid, taking the specified action. Useful
+     * for debugging.
+     */
+    // Port-added
+    public static void LLVMVerifyFunction(LLVMValueRef Fn, LLVMVerifierFailureAction Action) throws LLVMException {
+        if (nLLVMVerifyFunction(Fn, Action)) {
+            throw new LLVMException("Failed to verify function");
+        }
+    }
+
+    /**
+     * Verifies that a single function is valid. Useful for debugging.
+     */
+    // Port-added
+    public static void LLVMVerifyFunction(LLVMValueRef Fn) throws LLVMException {
+        LLVMVerifyFunction(Fn, LLVMVerifierFailureAction.LLVMPrintMessageAction);
+    }
+
+    /**
+     * Open up a ghostview window that displays the CFG of the current function.
+     * Useful for debugging.
+     */
+    public static void LLVMViewFunctionCFG(LLVMValueRef Fn) {
+        nothrows_run(() -> Function.LLVMViewFunctionCFG.handle().invoke(Fn.value()));
+    }
+
+    public static void LLVMViewFunctionCFGOnly(LLVMValueRef Fn) {
+        nothrows_run(() -> Function.LLVMViewFunctionCFGOnly.handle().invoke(Fn.value()));
+    }
+}
