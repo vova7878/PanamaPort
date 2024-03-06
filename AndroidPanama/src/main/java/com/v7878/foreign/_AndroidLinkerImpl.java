2d3c447aff3648126b4f9e3b2f2baaf4b373de24
v7878 <46866705+vova7878@users.noreply.github.com>
2024-03-06 14:57:53 +0700

    AndroidPanama: add aarch64 calling convention

--- AndroidPanama/src/main/java/com/v7878/foreign/_AndroidLinkerImpl.java
+++ AndroidPanama/src/main/java/com/v7878/foreign/_AndroidLinkerImpl.java
@@ -4,6 +4,7 @@ import static com.v7878.dex.DexConstants.ACC_NATIVE;
 import static com.v7878.dex.DexConstants.ACC_PRIVATE;
 import static com.v7878.dex.DexConstants.ACC_STATIC;
 import static com.v7878.foreign.MemoryLayout.PathElement.groupElement;
+import static com.v7878.foreign.MemoryLayout.sequenceLayout;
 import static com.v7878.foreign.MemoryLayout.structLayout;
 import static com.v7878.foreign.ValueLayout.ADDRESS;
 import static com.v7878.foreign.ValueLayout.JAVA_BYTE;
@@ -49,6 +50,10 @@ import static com.v7878.unsafe.AndroidUnsafe.putObject;
 import static com.v7878.unsafe.ArtMethodUtils.registerNativeMethod;
 import static com.v7878.unsafe.DexFileUtils.loadClass;
 import static com.v7878.unsafe.DexFileUtils.openDexFile;
+import static com.v7878.unsafe.NativeCodeBlob.CURRENT_INSTRUCTION_SET;
+import static com.v7878.unsafe.NativeCodeBlob.InstructionSet.ARM64;
+import static com.v7878.unsafe.NativeCodeBlob.InstructionSet.X86;
+import static com.v7878.unsafe.NativeCodeBlob.InstructionSet.X86_64;
 import static com.v7878.unsafe.Reflection.fieldOffset;
 import static com.v7878.unsafe.Reflection.getDeclaredField;
 import static com.v7878.unsafe.Reflection.getDeclaredMethod;
@@ -86,6 +91,7 @@ import com.v7878.llvm.ObjectFile;
 import com.v7878.llvm.Types.LLVMMemoryBufferRef;
 import com.v7878.llvm.Types.LLVMTypeRef;
 import com.v7878.llvm.Types.LLVMValueRef;
+import com.v7878.unsafe.AndroidUnsafe;
 import com.v7878.unsafe.NativeCodeBlob;
 import com.v7878.unsafe.Reflection;
 import com.v7878.unsafe.Utils;
@@ -398,7 +404,7 @@ final class _AndroidLinkerImpl extends _AbstractAndroidLinker {
         if (retLayout == null) {
             retType = VOID_T;
         } else if (retLayout instanceof GroupLayout gl) {
-            MemoryLayout wrapper = getGroupWrapper(gl);
+            MemoryLayout wrapper = getGroupWrapper(gl, true);
             if (wrapper != null) {
                 if (wrapper == VOID_WRAPPER) {
                     retType = VOID_T; // just drop
@@ -418,7 +424,7 @@ final class _AndroidLinkerImpl extends _AbstractAndroidLinker {
             if (layout instanceof AddressLayout) {
                 argTypes.add(getPointerType(layout));
             } else if (layout instanceof GroupLayout gl) {
-                MemoryLayout wrapper = getGroupWrapper(gl);
+                MemoryLayout wrapper = getGroupWrapper(gl, false);
                 if (wrapper != null) {
                     if (wrapper == VOID_WRAPPER) {
                         // just drop
@@ -494,7 +500,7 @@ final class _AndroidLinkerImpl extends _AbstractAndroidLinker {
                 if (retLayout == null) {
                     retVoid = true;
                 } else if (retLayout instanceof GroupLayout gl) {
-                    MemoryLayout retWrapper = getGroupWrapper(gl);
+                    MemoryLayout retWrapper = getGroupWrapper(gl, true);
                     if (retWrapper != null) {
                         int i = index++;
                         if (retWrapper == VOID_WRAPPER) {
@@ -520,7 +526,7 @@ final class _AndroidLinkerImpl extends _AbstractAndroidLinker {
                 while (index < args.length) {
                     MemoryLayout layout = f_descriptor.argumentLayouts().get(index - start);
                     if (layout instanceof GroupLayout gl) {
-                        MemoryLayout wrapper = getGroupWrapper(gl);
+                        MemoryLayout wrapper = getGroupWrapper(gl, false);
                         if (wrapper != null) {
                             if (wrapper == VOID_WRAPPER) {
                                 index++; // just drop
@@ -658,93 +664,190 @@ final class _AndroidLinkerImpl extends _AbstractAndroidLinker {
         throw new UnsupportedOperationException("Unsuppurted yet!");
     }
 
+    private static class x86_64_calling_convention {
+
+        private static void markWrapperType(WrapperType[] types, int start, int size, WrapperType type) {
+            for (int i = 0; i < size; i++) {
+                WrapperType oldType = types[start + i];
+                if (oldType == null || oldType.ordinal() < type.ordinal()) {
+                    types[start + i] = type;
+                }
+            }
+        }
+
+        private static void markWrapperType(MemoryLayout layout, int offset, WrapperType[] types) {
+            if (layout instanceof StructLayout sl) {
+                for (MemoryLayout member : sl.memberLayouts()) {
+                    markWrapperType(member, offset, types);
+                    offset = Math.addExact(offset, Math.toIntExact(member.byteSize()));
+                }
+            } else if (layout instanceof UnionLayout ul) {
+                for (MemoryLayout member : ul.memberLayouts()) {
+                    markWrapperType(member, offset, types);
+                }
+            } else if (layout instanceof SequenceLayout sl) {
+                MemoryLayout el = sl.elementLayout();
+                int count = Math.toIntExact(sl.elementCount());
+                int size = Math.toIntExact(el.byteSize());
+                for (int i = 0; i < count; i++) {
+                    markWrapperType(el, offset, types);
+                    offset = Math.addExact(offset, size);
+                }
+            } else if (layout instanceof ValueLayout vl) {
+                WrapperType type;
+                if (layout instanceof ValueLayout.OfFloat) {
+                    type = WrapperType.FLOAT;
+                } else if (layout instanceof ValueLayout.OfDouble) {
+                    type = WrapperType.DOUBLE;
+                } else {
+                    type = WrapperType.INT;
+                }
+                markWrapperType(types, offset, Math.toIntExact(vl.byteSize()), type);
+            } else if (layout instanceof PaddingLayout) {
+                // skip
+            } else {
+                throw shouldNotReachHere();
+            }
+        }
+
+        private static MemoryLayout getGroupWrapper(WrapperType[] types, int count) {
+            WrapperType max = null;
+            for (WrapperType type : types) {
+                if (max == null) {
+                    max = type;
+                }
+                if (type != null && type.ordinal() > max.ordinal()) {
+                    max = type;
+                }
+            }
+            return switch (count) {
+                case 0 -> VOID_WRAPPER;
+                case 1 -> JAVA_BYTE;
+                case 2 -> JAVA_SHORT;
+                case 3 -> JAVA_INT;
+                case 4 -> max == WrapperType.INT ? JAVA_INT : JAVA_FLOAT;
+                case 8 -> max == WrapperType.INT ? JAVA_LONG : (max == WrapperType.DOUBLE ?
+                        JAVA_DOUBLE : structLayout(JAVA_FLOAT, JAVA_FLOAT));
+                default -> JAVA_LONG;
+            };
+        }
+
+        public static MemoryLayout getGroupWrapper(GroupLayout layout) {
+            if (layout.byteSize() > 16 /* 128 bit */) return null;
+            WrapperType[] types = new WrapperType[16];
+            markWrapperType(layout, 0, types);
+            int count = 16;
+            while (count > 0 && types[count - 1] == null) {
+                count--;
+            }
+            WrapperType[] upper_half = Arrays.copyOf(types, 8);
+            if (count <= 8) {
+                return getGroupWrapper(upper_half, count);
+            }
+            WrapperType[] lower_half = Arrays.copyOfRange(types, 8, 16);
+            return structLayout(getGroupWrapper(upper_half, 8),
+                    getGroupWrapper(lower_half, count - 8));
+        }
+    }
+
+    private static class aarch64_calling_convention {
+        private static void markWrapperType(WrapperType[] types, int start, int size, WrapperType type) {
+            for (int i = 0; i < size; i++) {
+                WrapperType oldType = types[start + i];
+                if (oldType == null) {
+                    types[start + i] = type;
+                } else if (oldType.ordinal() != type.ordinal()) {
+                    types[start + i] = WrapperType.INT;
+                }
+            }
+        }
+
+        private static void markWrapperType(MemoryLayout layout, int offset, WrapperType[] types) {
+            if (layout instanceof StructLayout sl) {
+                for (MemoryLayout member : sl.memberLayouts()) {
+                    markWrapperType(member, offset, types);
+                    offset = Math.addExact(offset, Math.toIntExact(member.byteSize()));
+                }
+            } else if (layout instanceof UnionLayout ul) {
+                for (MemoryLayout member : ul.memberLayouts()) {
+                    markWrapperType(member, offset, types);
+                }
+            } else if (layout instanceof SequenceLayout sl) {
+                MemoryLayout el = sl.elementLayout();
+                int count = Math.toIntExact(sl.elementCount());
+                int size = Math.toIntExact(el.byteSize());
+                for (int i = 0; i < count; i++) {
+                    markWrapperType(el, offset, types);
+                    offset = Math.addExact(offset, size);
+                }
+            } else if (layout instanceof ValueLayout vl) {
+                WrapperType type;
+                if (layout instanceof ValueLayout.OfFloat) {
+                    type = WrapperType.FLOAT;
+                } else if (layout instanceof ValueLayout.OfDouble) {
+                    type = WrapperType.DOUBLE;
+                } else {
+                    type = WrapperType.INT;
+                }
+                markWrapperType(types, offset, Math.toIntExact(vl.byteSize()), type);
+            } else if (layout instanceof PaddingLayout) {
+                // skip
+            } else {
+                throw shouldNotReachHere();
+            }
+        }
+
+        public static MemoryLayout getGroupWrapper(GroupLayout layout) {
+            if (layout.byteSize() > 32 /* 256 bit */) return null;
+            WrapperType[] types = new WrapperType[32];
+            markWrapperType(layout, 0, types);
+            int count = 32;
+            while (count > 0 && types[count - 1] == null) {
+                count--;
+            }
+            types = Arrays.copyOfRange(types, 0, count);
+            WrapperType common = null;
+            for (WrapperType type : types) {
+                if (common == null) {
+                    common = type;
+                }
+                if (common != type) {
+                    common = WrapperType.INT;
+                    break;
+                }
+            }
+            common = common == null ? WrapperType.INT : common;
+            if (count > 16 /* 128 bit */ && common != WrapperType.DOUBLE) {
+                return null;
+            }
+            return switch (common) {
+                case INT -> count <= 8 ? JAVA_LONG : structLayout(JAVA_LONG, JAVA_LONG);
+                case DOUBLE -> sequenceLayout(count / 8, JAVA_DOUBLE);
+                case FLOAT -> sequenceLayout(count / 4, JAVA_FLOAT);
+                //noinspection UnnecessaryDefault
+                default -> throw shouldNotReachHere();
+            };
+        }
+    }
+
     private enum WrapperType {
         FLOAT, DOUBLE, INT
     }
 
-    private static void markWrapperType(WrapperType[] types, int start, int size, WrapperType type) {
-        for (int i = 0; i < size; i++) {
-            WrapperType oldType = types[start + i];
-            if (oldType == null || oldType.ordinal() < type.ordinal()) {
-                types[start + i] = type;
-            }
-        }
-    }
+    // Note: invalid layout
+    public static final MemoryLayout VOID_WRAPPER = AndroidUnsafe.allocateInstance(_PaddingLayoutImpl.class);
 
-    private static void markWrapperType(MemoryLayout layout, int offset, WrapperType[] types) {
-        if (layout instanceof StructLayout sl) {
-            for (MemoryLayout member : sl.memberLayouts()) {
-                markWrapperType(member, offset, types);
-                offset = Math.addExact(offset, Math.toIntExact(member.byteSize()));
-            }
-        } else if (layout instanceof UnionLayout ul) {
-            for (MemoryLayout member : ul.memberLayouts()) {
-                markWrapperType(member, offset, types);
-            }
-        } else if (layout instanceof SequenceLayout sl) {
-            MemoryLayout el = sl.elementLayout();
-            int count = Math.toIntExact(sl.elementCount());
-            int size = Math.toIntExact(el.byteSize());
-            for (int i = 0; i < count; i++) {
-                markWrapperType(el, offset, types);
-                offset = Math.addExact(offset, size);
-            }
-        } else if (layout instanceof ValueLayout vl) {
-            WrapperType type;
-            if (layout instanceof ValueLayout.OfFloat) {
-                type = WrapperType.FLOAT;
-            } else if (layout instanceof ValueLayout.OfDouble) {
-                type = WrapperType.DOUBLE;
-            } else {
-                type = WrapperType.INT;
-            }
-            markWrapperType(types, offset, Math.toIntExact(vl.byteSize()), type);
-        } else if (layout instanceof PaddingLayout) {
-            // skip
-        } else {
-            throw shouldNotReachHere();
+    public static MemoryLayout getGroupWrapper(GroupLayout layout, boolean is_return) {
+        if (CURRENT_INSTRUCTION_SET == X86) return null; // TODO: check
+        if (CURRENT_INSTRUCTION_SET == X86_64) {
+            return x86_64_calling_convention.getGroupWrapper(layout);
         }
-    }
-
-    private static MemoryLayout getGroupWrapper(WrapperType[] types, int count) {
-        WrapperType max = null;
-        for (WrapperType type : types) {
-            if (max == null) {
-                max = type;
-            }
-            if (type != null && type.ordinal() > max.ordinal()) {
-                max = type;
-            }
+        if (CURRENT_INSTRUCTION_SET == ARM64) {
+            // TODO: use is_return
+            // TODO: check
+            return aarch64_calling_convention.getGroupWrapper(layout);
         }
-        return switch (count) {
-            case 0 -> VOID_WRAPPER;
-            case 1 -> JAVA_BYTE;
-            case 2 -> JAVA_SHORT;
-            case 3 -> JAVA_INT;
-            case 4 -> max == WrapperType.INT ? JAVA_INT : JAVA_FLOAT;
-            case 8 -> max == WrapperType.INT ? JAVA_LONG : (max == WrapperType.DOUBLE ?
-                    JAVA_DOUBLE : structLayout(JAVA_FLOAT, JAVA_FLOAT));
-            default -> JAVA_LONG;
-        };
-    }
-
-    public static final MemoryLayout VOID_WRAPPER = structLayout();
-
-    public static MemoryLayout getGroupWrapper(GroupLayout layout) {
-        if (!IS64BIT) return null;
-        if (layout.byteSize() > 16 /* 128 bit */) return null;
-        WrapperType[] types = new WrapperType[16];
-        markWrapperType(layout, 0, types);
-        int count = 16;
-        while (count > 0 && types[count - 1] == null) {
-            count--;
-        }
-        WrapperType[] upper_half = Arrays.copyOf(types, 8);
-        if (count <= 8) {
-            return getGroupWrapper(upper_half, count);
-        }
-        WrapperType[] lower_half = Arrays.copyOfRange(types, 8, 16);
-        return structLayout(getGroupWrapper(upper_half, 8),
-                getGroupWrapper(lower_half, count - 8));
+        //TODO: arm, riscv64
+        throw new UnsupportedOperationException("Not supported yet!");
     }
 }
