fc7411f341f9159ef9f0e0d485bc354ba80615ad
v7878 <46866705+vova7878@users.noreply.github.com>
2024-03-20 13:49:39 +0700

    AndroidUnsafe: update DangerLevel annotation

--- AndroidUnsafe/src/main/java/com/v7878/unsafe/DangerLevel.java
+++ AndroidUnsafe/src/main/java/com/v7878/unsafe/DangerLevel.java
@@ -6,13 +6,13 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(value = RetentionPolicy.CLASS)
-@Target(value = {ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR})
+@Target(value = {ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.FIELD})
 public @interface DangerLevel {
 
     int VERY_CAREFUL = Integer.MAX_VALUE / 2;
     int POTENTIAL_GC_COLLISION = VERY_CAREFUL + 1;
     int GC_COLLISION_MOVABLE_OBJECTS = VERY_CAREFUL + 2;
-    //int BAD_GC_COLLISION = VERY_CAREFUL + 3;
+    int BAD_GC_COLLISION = VERY_CAREFUL + 3;
     int MAX = Integer.MAX_VALUE;
 
     int value();
