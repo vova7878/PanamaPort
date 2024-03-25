73f73aefb530e6d3d106200510386d69d4adb82f
v7878 <46866705+vova7878@users.noreply.github.com>
2024-03-25 17:04:35 +0700

    LLVM: ObjectFile: use BulkLinker

--- LLVM/src/main/java/com/v7878/llvm/ObjectFile.java
+++ LLVM/src/main/java/com/v7878/llvm/ObjectFile.java
@@ -1,26 +1,25 @@
 package com.v7878.llvm;
 
 import static com.v7878.llvm.LibLLVM.LLVM;
-import static com.v7878.llvm.LibLLVM.LLVM_SCOPE;
-import static com.v7878.llvm.Types.LLVMBool;
-import static com.v7878.llvm.Types.cLLVMMemoryBufferRef;
-import static com.v7878.llvm._Utils.CONST_CHAR_PTR;
-import static com.v7878.llvm._Utils.UINT64_T;
-import static com.v7878.llvm._Utils.VOID_PTR;
 import static com.v7878.llvm._Utils.addressToLLVMString;
 import static com.v7878.llvm._Utils.addressToString;
-import static com.v7878.unsafe.Utils.nothrows_run;
-import static com.v7878.unsafe.foreign.SimpleLinker.processSymbol;
+import static com.v7878.unsafe.foreign.BulkLinker.CallType.CRITICAL;
+import static com.v7878.unsafe.foreign.BulkLinker.MapType.BOOL_AS_INT;
+import static com.v7878.unsafe.foreign.BulkLinker.MapType.LONG;
+import static com.v7878.unsafe.foreign.BulkLinker.MapType.LONG_AS_WORD;
+import static com.v7878.unsafe.foreign.BulkLinker.MapType.VOID;
 
+import androidx.annotation.Keep;
+
+import com.v7878.foreign.Arena;
 import com.v7878.foreign.MemorySegment;
 import com.v7878.llvm.Types.AddressValue;
 import com.v7878.llvm.Types.LLVMMemoryBufferRef;
+import com.v7878.unsafe.AndroidUnsafe;
 import com.v7878.unsafe.Utils.FineClosable;
-
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodType;
-import java.util.Objects;
-import java.util.function.Supplier;
+import com.v7878.unsafe.foreign.BulkLinker;
+import com.v7878.unsafe.foreign.BulkLinker.CallSignature;
+import com.v7878.unsafe.foreign.BulkLinker.LibrarySymbol;
 
 public class ObjectFile {
 
@@ -28,11 +27,6 @@ public class ObjectFile {
      * @defgroup LLVMCObject Object file reading and writing
      * @ingroup LLVMC
      */
-
-    static final Class<?> cLLVMObjectFileRef = VOID_PTR;
-    static final Class<?> cLLVMSectionIteratorRef = VOID_PTR;
-    static final Class<?> cLLVMSymbolIteratorRef = VOID_PTR;
-    static final Class<?> cLLVMRelocationIteratorRef = VOID_PTR;
 
     public static final class LLVMObjectFileRef extends AddressValue implements FineClosable {
 
@@ -126,123 +120,191 @@ public class ObjectFile {
         }
     }
 
-    private enum Function {
-        LLVMCreateObjectFile(cLLVMObjectFileRef, cLLVMMemoryBufferRef),
-        LLVMDisposeObjectFile(void.class, cLLVMObjectFileRef),
-        LLVMGetSections(cLLVMSectionIteratorRef, cLLVMObjectFileRef),
-        LLVMDisposeSectionIterator(void.class, cLLVMSectionIteratorRef),
-        LLVMIsSectionIteratorAtEnd(LLVMBool, cLLVMObjectFileRef, cLLVMSectionIteratorRef),
-        LLVMMoveToNextSection(void.class, cLLVMSectionIteratorRef),
-        LLVMMoveToContainingSection(void.class, cLLVMSectionIteratorRef, cLLVMSymbolIteratorRef),
-        LLVMGetSymbols(cLLVMSymbolIteratorRef, cLLVMObjectFileRef),
-        LLVMDisposeSymbolIterator(void.class, cLLVMSymbolIteratorRef),
-        LLVMIsSymbolIteratorAtEnd(LLVMBool, cLLVMObjectFileRef, cLLVMSymbolIteratorRef),
-        LLVMMoveToNextSymbol(void.class, cLLVMSymbolIteratorRef),
-        LLVMGetSectionName(CONST_CHAR_PTR, cLLVMSectionIteratorRef),
-        LLVMGetSectionSize(UINT64_T, cLLVMSectionIteratorRef),
-        LLVMGetSectionContents(CONST_CHAR_PTR, cLLVMSectionIteratorRef),
-        LLVMGetSectionAddress(UINT64_T, cLLVMSectionIteratorRef),
-        LLVMGetSectionContainsSymbol(LLVMBool, cLLVMSectionIteratorRef, cLLVMSymbolIteratorRef),
-        LLVMGetRelocations(cLLVMRelocationIteratorRef, cLLVMSectionIteratorRef),
-        LLVMDisposeRelocationIterator(void.class, cLLVMRelocationIteratorRef),
-        LLVMIsRelocationIteratorAtEnd(LLVMBool, cLLVMSectionIteratorRef, cLLVMRelocationIteratorRef),
-        LLVMMoveToNextRelocation(void.class, cLLVMRelocationIteratorRef),
-        LLVMGetSymbolName(CONST_CHAR_PTR, cLLVMSymbolIteratorRef),
-        LLVMGetSymbolAddress(UINT64_T, cLLVMSymbolIteratorRef),
-        LLVMGetSymbolSize(UINT64_T, cLLVMSymbolIteratorRef),
-        LLVMGetRelocationOffset(UINT64_T, cLLVMRelocationIteratorRef),
-        LLVMGetRelocationSymbol(cLLVMSymbolIteratorRef, cLLVMRelocationIteratorRef),
-        LLVMGetRelocationType(UINT64_T, cLLVMRelocationIteratorRef),
-        LLVMGetRelocationTypeName(CONST_CHAR_PTR, cLLVMRelocationIteratorRef),
-        LLVMGetRelocationValueString(CONST_CHAR_PTR, cLLVMRelocationIteratorRef);
+    @SuppressWarnings("unused")
+    @Keep
+    private abstract static class Native {
 
-        private final MethodType type;
-        private final Supplier<MethodHandle> handle;
+        private static final Arena SCOPE = Arena.ofAuto();
 
-        Function(Class<?> rtype, Class<?>... atypes) {
-            this.type = MethodType.methodType(rtype, atypes);
-            this.handle = processSymbol(LLVM, LLVM_SCOPE, name(), type());
-        }
+        @LibrarySymbol("LLVMCreateObjectFile")
+        @CallSignature(type = CRITICAL, ret = LONG_AS_WORD, args = {LONG_AS_WORD})
+        abstract long LLVMCreateObjectFile(long MemBuf);
 
-        public MethodType type() {
-            return type;
-        }
+        @LibrarySymbol("LLVMDisposeObjectFile")
+        @CallSignature(type = CRITICAL, ret = VOID, args = {LONG_AS_WORD})
+        abstract void LLVMDisposeObjectFile(long ObjectFile);
 
-        public MethodHandle handle() {
-            return Objects.requireNonNull(handle.get());
-        }
+        @LibrarySymbol("LLVMGetSections")
+        @CallSignature(type = CRITICAL, ret = LONG_AS_WORD, args = {LONG_AS_WORD})
+        abstract long LLVMGetSections(long ObjectFile);
 
-        @Override
-        public String toString() {
-            return name() + "{" +
-                    "type=" + type +
-                    ", handle=" + handle() + '}';
-        }
+        @LibrarySymbol("LLVMDisposeSectionIterator")
+        @CallSignature(type = CRITICAL, ret = VOID, args = {LONG_AS_WORD})
+        abstract void LLVMDisposeSectionIterator(long SI);
+
+        @LibrarySymbol("LLVMIsSectionIteratorAtEnd")
+        @CallSignature(type = CRITICAL, ret = BOOL_AS_INT, args = {LONG_AS_WORD, LONG_AS_WORD})
+        abstract boolean LLVMIsSectionIteratorAtEnd(long ObjectFile, long SI);
+
+        @LibrarySymbol("LLVMMoveToNextSection")
+        @CallSignature(type = CRITICAL, ret = VOID, args = {LONG_AS_WORD})
+        abstract void LLVMMoveToNextSection(long SI);
+
+        @LibrarySymbol("LLVMMoveToContainingSection")
+        @CallSignature(type = CRITICAL, ret = VOID, args = {LONG_AS_WORD, LONG_AS_WORD})
+        abstract void LLVMMoveToContainingSection(long Sect, long Sym);
+
+        @LibrarySymbol("LLVMGetSymbols")
+        @CallSignature(type = CRITICAL, ret = LONG_AS_WORD, args = {LONG_AS_WORD})
+        abstract long LLVMGetSymbols(long ObjectFile);
+
+        @LibrarySymbol("LLVMDisposeSymbolIterator")
+        @CallSignature(type = CRITICAL, ret = VOID, args = {LONG_AS_WORD})
+        abstract void LLVMDisposeSymbolIterator(long SI);
+
+        @LibrarySymbol("LLVMIsSymbolIteratorAtEnd")
+        @CallSignature(type = CRITICAL, ret = BOOL_AS_INT, args = {LONG_AS_WORD, LONG_AS_WORD})
+        abstract boolean LLVMIsSymbolIteratorAtEnd(long ObjectFile, long SI);
+
+        @LibrarySymbol("LLVMMoveToNextSymbol")
+        @CallSignature(type = CRITICAL, ret = VOID, args = {LONG_AS_WORD})
+        abstract void LLVMMoveToNextSymbol(long SI);
+
+        @LibrarySymbol("LLVMGetSectionName")
+        @CallSignature(type = CRITICAL, ret = LONG_AS_WORD, args = {LONG_AS_WORD})
+        abstract long LLVMGetSectionName(long SI);
+
+        @LibrarySymbol("LLVMGetSectionSize")
+        @CallSignature(type = CRITICAL, ret = LONG, args = {LONG_AS_WORD})
+        abstract long LLVMGetSectionSize(long SI);
+
+        @LibrarySymbol("LLVMGetSectionContents")
+        @CallSignature(type = CRITICAL, ret = LONG_AS_WORD, args = {LONG_AS_WORD})
+        abstract long LLVMGetSectionContents(long SI);
+
+        @LibrarySymbol("LLVMGetSectionAddress")
+        @CallSignature(type = CRITICAL, ret = LONG, args = {LONG_AS_WORD})
+        abstract long LLVMGetSectionAddress(long SI);
+
+        @LibrarySymbol("LLVMGetSectionContainsSymbol")
+        @CallSignature(type = CRITICAL, ret = BOOL_AS_INT, args = {LONG_AS_WORD, LONG_AS_WORD})
+        abstract boolean LLVMGetSectionContainsSymbol(long SI, long Sym);
+
+        @LibrarySymbol("LLVMGetRelocations")
+        @CallSignature(type = CRITICAL, ret = LONG_AS_WORD, args = {LONG_AS_WORD})
+        abstract long LLVMGetRelocations(long Section);
+
+        @LibrarySymbol("LLVMDisposeRelocationIterator")
+        @CallSignature(type = CRITICAL, ret = VOID, args = {LONG_AS_WORD})
+        abstract void LLVMDisposeRelocationIterator(long RI);
+
+        @LibrarySymbol("LLVMIsRelocationIteratorAtEnd")
+        @CallSignature(type = CRITICAL, ret = BOOL_AS_INT, args = {LONG_AS_WORD, LONG_AS_WORD})
+        abstract boolean LLVMIsRelocationIteratorAtEnd(long Section, long RI);
+
+        @LibrarySymbol("LLVMMoveToNextRelocation")
+        @CallSignature(type = CRITICAL, ret = VOID, args = {LONG_AS_WORD})
+        abstract void LLVMMoveToNextRelocation(long RI);
+
+        @LibrarySymbol("LLVMGetSymbolName")
+        @CallSignature(type = CRITICAL, ret = LONG_AS_WORD, args = {LONG_AS_WORD})
+        abstract long LLVMGetSymbolName(long SI);
+
+        @LibrarySymbol("LLVMGetSymbolAddress")
+        @CallSignature(type = CRITICAL, ret = LONG, args = {LONG_AS_WORD})
+        abstract long LLVMGetSymbolAddress(long SI);
+
+        @LibrarySymbol("LLVMGetSymbolSize")
+        @CallSignature(type = CRITICAL, ret = LONG, args = {LONG_AS_WORD})
+        abstract long LLVMGetSymbolSize(long SI);
+
+        @LibrarySymbol("LLVMGetRelocationOffset")
+        @CallSignature(type = CRITICAL, ret = LONG, args = {LONG_AS_WORD})
+        abstract long LLVMGetRelocationOffset(long RI);
+
+        @LibrarySymbol("LLVMGetRelocationSymbol")
+        @CallSignature(type = CRITICAL, ret = LONG_AS_WORD, args = {LONG_AS_WORD})
+        abstract long LLVMGetRelocationSymbol(long RI);
+
+        @LibrarySymbol("LLVMGetRelocationType")
+        @CallSignature(type = CRITICAL, ret = LONG, args = {LONG_AS_WORD})
+        abstract long LLVMGetRelocationType(long RI);
+
+        @LibrarySymbol("LLVMGetRelocationTypeName")
+        @CallSignature(type = CRITICAL, ret = LONG_AS_WORD, args = {LONG_AS_WORD})
+        abstract long LLVMGetRelocationTypeName(long RI);
+
+        @LibrarySymbol("LLVMGetRelocationValueString")
+        @CallSignature(type = CRITICAL, ret = LONG_AS_WORD, args = {LONG_AS_WORD})
+        abstract long LLVMGetRelocationValueString(long RI);
+
+        static final Native INSTANCE = AndroidUnsafe.allocateInstance(
+                BulkLinker.processSymbols(SCOPE, Native.class, LLVM));
     }
 
     // ObjectFile creation
 
     public static LLVMObjectFileRef LLVMCreateObjectFile(LLVMMemoryBufferRef MemBuf) {
-        return nothrows_run(() -> LLVMObjectFileRef.ofNullable((long) Function.LLVMCreateObjectFile.handle().invoke(MemBuf.value())));
+        return LLVMObjectFileRef.ofNullable(Native.INSTANCE.LLVMCreateObjectFile(MemBuf.value()));
     }
 
     public static void LLVMDisposeObjectFile(LLVMObjectFileRef ObjectFile) {
-        nothrows_run(() -> Function.LLVMDisposeObjectFile.handle().invoke(ObjectFile.value()));
+        Native.INSTANCE.LLVMDisposeObjectFile(ObjectFile.value());
     }
 
     // ObjectFile Section iterators
 
     public static LLVMSectionIteratorRef LLVMGetSections(LLVMObjectFileRef ObjectFile) {
-        return nothrows_run(() -> LLVMSectionIteratorRef.ofNullable((long) Function.LLVMGetSections.handle().invoke(ObjectFile.value())));
+        return LLVMSectionIteratorRef.ofNullable(Native.INSTANCE.LLVMGetSections(ObjectFile.value()));
     }
 
     public static void LLVMDisposeSectionIterator(LLVMSectionIteratorRef SI) {
-        nothrows_run(() -> Function.LLVMDisposeSectionIterator.handle().invoke(SI.value()));
+        Native.INSTANCE.LLVMDisposeSectionIterator(SI.value());
     }
 
     public static boolean LLVMIsSectionIteratorAtEnd(LLVMObjectFileRef ObjectFile, LLVMSectionIteratorRef SI) {
-        return nothrows_run(() -> (boolean) Function.LLVMIsSectionIteratorAtEnd.handle().invoke(ObjectFile.value(), SI.value()));
+        return Native.INSTANCE.LLVMIsSectionIteratorAtEnd(ObjectFile.value(), SI.value());
     }
 
     public static void LLVMMoveToNextSection(LLVMSectionIteratorRef SI) {
-        nothrows_run(() -> Function.LLVMMoveToNextSection.handle().invoke(SI.value()));
+        Native.INSTANCE.LLVMMoveToNextSection(SI.value());
     }
 
     public static void LLVMMoveToContainingSection(LLVMSectionIteratorRef Sect, LLVMSymbolIteratorRef Sym) {
-        nothrows_run(() -> Function.LLVMMoveToContainingSection.handle().invoke(Sect.value(), Sym.value()));
+        Native.INSTANCE.LLVMMoveToContainingSection(Sect.value(), Sym.value());
     }
 
     // ObjectFile Symbol iterators
 
     public static LLVMSymbolIteratorRef LLVMGetSymbols(LLVMObjectFileRef ObjectFile) {
-        return nothrows_run(() -> LLVMSymbolIteratorRef.ofNullable((long) Function.LLVMGetSymbols.handle().invoke(ObjectFile.value())));
+        return LLVMSymbolIteratorRef.ofNullable(Native.INSTANCE.LLVMGetSymbols(ObjectFile.value()));
     }
 
     public static void LLVMDisposeSymbolIterator(LLVMSymbolIteratorRef SI) {
-        nothrows_run(() -> Function.LLVMDisposeSymbolIterator.handle().invoke(SI.value()));
+        Native.INSTANCE.LLVMDisposeSymbolIterator(SI.value());
     }
 
     public static boolean LLVMIsSymbolIteratorAtEnd(LLVMObjectFileRef ObjectFile, LLVMSymbolIteratorRef SI) {
-        return nothrows_run(() -> (boolean) Function.LLVMIsSymbolIteratorAtEnd.handle().invoke(ObjectFile.value(), SI.value()));
+        return Native.INSTANCE.LLVMIsSymbolIteratorAtEnd(ObjectFile.value(), SI.value());
     }
 
     public static void LLVMMoveToNextSymbol(LLVMSymbolIteratorRef SI) {
-        nothrows_run(() -> Function.LLVMMoveToNextSymbol.handle().invoke(SI.value()));
+        Native.INSTANCE.LLVMMoveToNextSymbol(SI.value());
     }
 
     // SectionRef accessors
 
     public static String LLVMGetSectionName(LLVMSectionIteratorRef SI) {
-        return nothrows_run(() -> addressToString((long) Function.LLVMGetSectionName.handle().invoke(SI.value())));
+        return addressToString(Native.INSTANCE.LLVMGetSectionName(SI.value()));
     }
 
     public static long /* uint64_t */ LLVMGetSectionSize(LLVMSectionIteratorRef SI) {
-        return nothrows_run(() -> (long) Function.LLVMGetSectionSize.handle().invoke(SI.value()));
+        return Native.INSTANCE.LLVMGetSectionSize(SI.value());
     }
 
     /* package-private */
     static long LLVMGetSectionContents(LLVMSectionIteratorRef SI) {
-        return nothrows_run(() -> (long) Function.LLVMGetSectionContents.handle().invoke(SI.value()));
+        return Native.INSTANCE.LLVMGetSectionContents(SI.value());
     }
 
     // Port-added
@@ -253,57 +315,57 @@ public class ObjectFile {
     }
 
     public static long /* uint64_t */ LLVMGetSectionAddress(LLVMSectionIteratorRef SI) {
-        return nothrows_run(() -> (long) Function.LLVMGetSectionAddress.handle().invoke(SI.value()));
+        return Native.INSTANCE.LLVMGetSectionAddress(SI.value());
     }
 
     public static boolean LLVMGetSectionContainsSymbol(LLVMSectionIteratorRef SI, LLVMSymbolIteratorRef Sym) {
-        return nothrows_run(() -> (boolean) Function.LLVMGetSectionContainsSymbol.handle().invoke(SI.value(), Sym.value()));
+        return Native.INSTANCE.LLVMGetSectionContainsSymbol(SI.value(), Sym.value());
     }
 
     // Section Relocation iterators
 
     public static LLVMRelocationIteratorRef LLVMGetRelocations(LLVMSectionIteratorRef Section) {
-        return nothrows_run(() -> LLVMRelocationIteratorRef.ofNullable((long) Function.LLVMGetRelocations.handle().invoke(Section.value())));
+        return LLVMRelocationIteratorRef.ofNullable(Native.INSTANCE.LLVMGetRelocations(Section.value()));
     }
 
     public static void LLVMDisposeRelocationIterator(LLVMRelocationIteratorRef RI) {
-        nothrows_run(() -> Function.LLVMDisposeRelocationIterator.handle().invoke(RI.value()));
+        Native.INSTANCE.LLVMDisposeRelocationIterator(RI.value());
     }
 
     public static boolean LLVMIsRelocationIteratorAtEnd(LLVMSectionIteratorRef Section, LLVMRelocationIteratorRef RI) {
-        return nothrows_run(() -> (boolean) Function.LLVMIsRelocationIteratorAtEnd.handle().invoke(Section.value(), RI.value()));
+        return Native.INSTANCE.LLVMIsRelocationIteratorAtEnd(Section.value(), RI.value());
     }
 
     public static void LLVMMoveToNextRelocation(LLVMRelocationIteratorRef RI) {
-        nothrows_run(() -> Function.LLVMMoveToNextRelocation.handle().invoke(RI.value()));
+        Native.INSTANCE.LLVMMoveToNextRelocation(RI.value());
     }
 
     // SymbolRef accessors
 
     public static String LLVMGetSymbolName(LLVMSymbolIteratorRef SI) {
-        return nothrows_run(() -> addressToString((long) Function.LLVMGetSymbolName.handle().invoke(SI.value())));
+        return addressToString(Native.INSTANCE.LLVMGetSymbolName(SI.value()));
     }
 
     public static long /* uint64_t */ LLVMGetSymbolAddress(LLVMSymbolIteratorRef SI) {
-        return nothrows_run(() -> (long) Function.LLVMGetSymbolAddress.handle().invoke(SI.value()));
+        return Native.INSTANCE.LLVMGetSymbolAddress(SI.value());
     }
 
     public static long /* uint64_t */ LLVMGetSymbolSize(LLVMSymbolIteratorRef SI) {
-        return nothrows_run(() -> (long) Function.LLVMGetSymbolSize.handle().invoke(SI.value()));
+        return Native.INSTANCE.LLVMGetSymbolSize(SI.value());
     }
 
     // RelocationRef accessors
 
     public static long /* uint64_t */ LLVMGetRelocationOffset(LLVMRelocationIteratorRef RI) {
-        return nothrows_run(() -> (long) Function.LLVMGetRelocationOffset.handle().invoke(RI.value()));
+        return Native.INSTANCE.LLVMGetRelocationOffset(RI.value());
     }
 
     public static LLVMSymbolIteratorRef LLVMGetRelocationSymbol(LLVMRelocationIteratorRef RI) {
-        return nothrows_run(() -> LLVMSymbolIteratorRef.ofNullable((long) Function.LLVMGetRelocationSymbol.handle().invoke(RI.value())));
+        return LLVMSymbolIteratorRef.ofNullable(Native.INSTANCE.LLVMGetRelocationSymbol(RI.value()));
     }
 
     public static long /* uint64_t */ LLVMGetRelocationType(LLVMRelocationIteratorRef RI) {
-        return nothrows_run(() -> (long) Function.LLVMGetRelocationType.handle().invoke(RI.value()));
+        return Native.INSTANCE.LLVMGetRelocationType(RI.value());
     }
 
     // NOTE: Caller takes ownership of returned string of the two
@@ -311,11 +373,11 @@ public class ObjectFile {
 
     public static String LLVMGetRelocationTypeName(LLVMRelocationIteratorRef RI) {
         // NOTE: using addressToLLVMString is wrong, but it just frees the pointer, that's ok
-        return nothrows_run(() -> addressToLLVMString((long) Function.LLVMGetRelocationTypeName.handle().invoke(RI.value())));
+        return addressToLLVMString(Native.INSTANCE.LLVMGetRelocationTypeName(RI.value()));
     }
 
     public static String LLVMGetRelocationValueString(LLVMRelocationIteratorRef RI) {
         // NOTE: using addressToLLVMString is wrong, but it just frees the pointer, that's ok
-        return nothrows_run(() -> addressToLLVMString((long) Function.LLVMGetRelocationValueString.handle().invoke(RI.value())));
+        return addressToLLVMString(Native.INSTANCE.LLVMGetRelocationValueString(RI.value()));
     }
 }
