6d10fbacb251ea354f49cd333560d3f89adb0929
v7878 <46866705+vova7878@users.noreply.github.com>
2024-07-29 15:14:55 +0700

    AndroidUnsafe: MemoryOperators: fix for 32-bit architectures

--- AndroidUnsafe/src/main/java/com/v7878/unsafe/cpp_std/MemoryOperators.java
+++ AndroidUnsafe/src/main/java/com/v7878/unsafe/cpp_std/MemoryOperators.java
@@ -1,6 +1,11 @@
 package com.v7878.unsafe.cpp_std;
 
+import static com.v7878.unsafe.InstructionSet.ARM;
+import static com.v7878.unsafe.InstructionSet.ARM64;
 import static com.v7878.unsafe.InstructionSet.CURRENT_INSTRUCTION_SET;
+import static com.v7878.unsafe.InstructionSet.RISCV64;
+import static com.v7878.unsafe.InstructionSet.X86;
+import static com.v7878.unsafe.InstructionSet.X86_64;
 import static com.v7878.unsafe.foreign.BulkLinker.CallType.CRITICAL;
 import static com.v7878.unsafe.foreign.BulkLinker.MapType.LONG_AS_WORD;
 import static com.v7878.unsafe.foreign.BulkLinker.MapType.VOID;
@@ -11,6 +16,7 @@ import com.v7878.foreign.Arena;
 import com.v7878.unsafe.AndroidUnsafe;
 import com.v7878.unsafe.foreign.BulkLinker;
 import com.v7878.unsafe.foreign.BulkLinker.CallSignature;
+import com.v7878.unsafe.foreign.BulkLinker.Conditions;
 import com.v7878.unsafe.foreign.BulkLinker.LibrarySymbol;
 
 public class MemoryOperators {
@@ -21,24 +27,32 @@ public class MemoryOperators {
     private abstract static class Native {
         private static final Arena SCOPE = Arena.ofAuto();
 
-        @LibrarySymbol(name = "_ZnamSt11align_val_tRKSt9nothrow_t")
-        @CallSignature(type = CRITICAL, ret = LONG_AS_WORD, args = {LONG_AS_WORD, LONG_AS_WORD})
+        // operator new[](unsigned int, std::align_val_t, std::nothrow_t const&)
+        @LibrarySymbol(conditions = @Conditions(arch = {ARM, X86}),
+                name = "_ZnajSt11align_val_tRKSt9nothrow_t")
         // operator new[](unsigned long, std::align_val_t, std::nothrow_t const&)
+        @LibrarySymbol(conditions = @Conditions(arch = {ARM64, X86_64, RISCV64}),
+                name = "_ZnamSt11align_val_tRKSt9nothrow_t")
+        @CallSignature(type = CRITICAL, ret = LONG_AS_WORD, args = {LONG_AS_WORD, LONG_AS_WORD})
         abstract long new_(long count, long alignment);
 
-        @LibrarySymbol(name = "_ZnamRKSt9nothrow_t")
-        @CallSignature(type = CRITICAL, ret = LONG_AS_WORD, args = {LONG_AS_WORD})
+        // operator new[](unsigned int, std::nothrow_t const&)
+        @LibrarySymbol(conditions = @Conditions(arch = {ARM, X86}),
+                name = "_ZnajRKSt9nothrow_t")
         // operator new[](unsigned long, std::nothrow_t const&)
+        @LibrarySymbol(conditions = @Conditions(arch = {ARM64, X86_64, RISCV64}),
+                name = "_ZnamRKSt9nothrow_t")
+        @CallSignature(type = CRITICAL, ret = LONG_AS_WORD, args = {LONG_AS_WORD})
         abstract long new_(long count);
 
+        // operator delete[](void*, std::align_val_t)
         @LibrarySymbol(name = "_ZdaPvSt11align_val_t")
         @CallSignature(type = CRITICAL, ret = VOID, args = {LONG_AS_WORD, LONG_AS_WORD})
-        // operator delete[](void*, std::align_val_t)
         abstract void delete(long ptr, long align);
 
+        // operator delete[](void*)
         @LibrarySymbol(name = "_ZdaPv")
         @CallSignature(type = CRITICAL, ret = VOID, args = {LONG_AS_WORD})
-        // operator delete[](void*)
         abstract void delete(long ptr);
 
         static final Native INSTANCE = AndroidUnsafe.allocateInstance(
