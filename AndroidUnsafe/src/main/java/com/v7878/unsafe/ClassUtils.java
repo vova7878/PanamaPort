1a5991f3541c5adda8191d8e3b91145ca7c1497d
v7878 <46866705+vova7878@users.noreply.github.com>
2024-08-24 22:50:35 +0700

    AndroidUnsafe: delete ClassUtils.kAccSkipHiddenApiChecks

--- AndroidUnsafe/src/main/java/com/v7878/unsafe/ClassUtils.java
+++ AndroidUnsafe/src/main/java/com/v7878/unsafe/ClassUtils.java
@@ -200,13 +200,4 @@ public class ClassUtils {
     public static boolean isClassInitialized(Class<?> clazz) {
         return getRawClassStatus(clazz) >= ClassStatus.Initialized.rawValue();
     }
-
-    @ApiSensitive
-    public static final int kAccSkipHiddenApiChecks = switch (CORRECT_SDK_INT) {
-        case 35 /*android 15*/, 34 /*android 14*/, 33 /*android 13*/,
-             32 /*android 12L*/, 31 /*android 12*/, 30 /*android 11*/,
-             29 /*android 10*/, 28 /*android 9*/ -> 0x00100000;
-        case 27 /*android 8.1*/, 26 /*android 8*/ -> 0;
-        default -> throw new IllegalStateException("unsupported sdk: " + CORRECT_SDK_INT);
-    };
 }
