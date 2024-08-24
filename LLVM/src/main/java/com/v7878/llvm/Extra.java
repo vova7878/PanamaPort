e5b09d65ee4866e1552ae02396976cd8633f3b60
v7878 <46866705+vova7878@users.noreply.github.com>
2024-08-24 21:14:08 +0700

    LLVM: refactor Extra class

--- LLVM/src/main/java/com/v7878/llvm/Extra.java
+++ LLVM/src/main/java/com/v7878/llvm/Extra.java
@@ -1,17 +1,13 @@
 package com.v7878.llvm;
 
-import static com.v7878.unsafe.InstructionSet.ARM;
-import static com.v7878.unsafe.InstructionSet.ARM64;
 import static com.v7878.unsafe.InstructionSet.CURRENT_INSTRUCTION_SET;
-import static com.v7878.unsafe.InstructionSet.X86;
-import static com.v7878.unsafe.InstructionSet.X86_64;
 
 public final class Extra {
     private Extra() {
     }
 
     public static String LLVMGetHostTriple() {
-        // TODO
+        // TODO _ZN4llvm3sys16getProcessTripleEv
         return LLVMGetHostCPUName() + "-unknown-linux-android";
     }
 
@@ -30,14 +26,14 @@ public final class Extra {
 
     public static String LLVMGetHostCPUFeatures() {
         // TODO _ZN4llvm3sys18getHostCPUFeaturesERNS_9StringMapIbNS_15MallocAllocatorEEE
-        if (CURRENT_INSTRUCTION_SET == X86)
-            return "+x87,+mmx,+sse,+sse2,+sse3,+ssse3";
-        if (CURRENT_INSTRUCTION_SET == X86_64)
-            return "+x87,+mmx,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt";
-        if (CURRENT_INSTRUCTION_SET == ARM64)
-            return "+fp-armv8,+neon,+v8a,+reserve-x18";
-        if (CURRENT_INSTRUCTION_SET == ARM)
-            return "+armv7-a,+neon,-thumb-mode,+soft-float-abi";
-        return "";
+        return switch (CURRENT_INSTRUCTION_SET) {
+            case X86 -> "+x87,+mmx,+sse,+sse2,+sse3,+ssse3";
+            case X86_64 -> "+x87,+mmx,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt";
+            case ARM -> "+armv7-a,+neon,-thumb-mode,+soft-float-abi";
+            case ARM64 -> "+fp-armv8,+neon,+v8a,+reserve-x18";
+            //TODO: RISCV64
+            default -> throw new IllegalStateException(
+                    "unsupported instruction set: " + CURRENT_INSTRUCTION_SET);
+        };
     }
 }
