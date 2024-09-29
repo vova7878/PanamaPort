c6130371b6ccb42afd142670509d316546555bef
v7878 <46866705+vova7878@users.noreply.github.com>
2024-09-29 16:47:34 +0700

    AndroidUnsafe: refactor annotations

--- AndroidUnsafe/src/main/java/com/v7878/unsafe/DangerLevel.java
+++ AndroidUnsafe/src/main/java/com/v7878/unsafe/DangerLevel.java
@@ -5,8 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(value = RetentionPolicy.CLASS)
-@Target(value = {ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.FIELD})
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.FIELD})
 public @interface DangerLevel {
 
     int VERY_CAREFUL = Integer.MAX_VALUE / 2;
--- AndroidUnsafe/src/main/java/com/v7878/unsafe/RawOffset.java
+++ AndroidUnsafe/src/main/java/com/v7878/unsafe/RawOffset.java
@@ -1,12 +1,11 @@
 package com.v7878.unsafe;
 
-import static java.lang.annotation.ElementType.FIELD;
-
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(FIELD)
 @Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.FIELD)
 public @interface RawOffset {
 }
