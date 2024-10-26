818824f80b4e0f43346e53608212d44924423e2f
v7878 <46866705+vova7878@users.noreply.github.com>
2024-10-26 13:48:03 +0700

    AndroidUnsafe: fix MappedMemoryUtils.isLoaded

--- AndroidUnsafe/src/main/java/com/v7878/unsafe/access/MappedMemoryUtils.java
+++ AndroidUnsafe/src/main/java/com/v7878/unsafe/access/MappedMemoryUtils.java
@@ -1,5 +1,6 @@
 package com.v7878.unsafe.access;
 
+import static com.v7878.misc.Version.CORRECT_SDK_INT;
 import static com.v7878.unsafe.ArtMethodUtils.getExecutableData;
 import static com.v7878.unsafe.ArtMethodUtils.registerNativeMethod;
 import static com.v7878.unsafe.Reflection.getDeclaredMethods;
@@ -26,7 +27,12 @@ class MappedMemoryUtils {
         long offset = mappingOffset(address);
         address -= offset;
         length += offset;
-        return isLoaded0(address, length, (int) pageCount(length));
+        long pc = pageCount(length);
+        if (CORRECT_SDK_INT < 35) {
+            return isLoaded0_before35(address, length, (int) pc);
+        } else {
+            return isLoaded0_after35(address, length, pc);
+        }
     }
 
     // not used, but a potential target for a store, see load() for details.
@@ -96,7 +102,11 @@ class MappedMemoryUtils {
 
     @DoNotShrink
     @DoNotObfuscate
-    private static native boolean isLoaded0(long address, long length, int pageCount);
+    private static native boolean isLoaded0_before35(long address, long length, int pageCount);
+
+    @DoNotShrink
+    @DoNotObfuscate
+    private static native boolean isLoaded0_after35(long address, long length, long pageCount);
 
     @DoNotShrink
     @DoNotObfuscate
@@ -105,8 +115,10 @@ class MappedMemoryUtils {
     static {
         Method[] tm = getDeclaredMethods(MappedMemoryUtils.class);
         Method[] mm = getDeclaredMethods(MappedByteBuffer.class);
-        registerNativeMethod(searchMethod(tm, "isLoaded0", long.class, long.class, int.class),
-                getExecutableData(searchMethod(mm, "isLoaded0", long.class, long.class, int.class)));
+        Class<?> pc_type = CORRECT_SDK_INT < 35 ? int.class : long.class;
+        String suffix = CORRECT_SDK_INT < 35 ? "_before35" : "_after35";
+        registerNativeMethod(searchMethod(tm, "isLoaded0" + suffix, long.class, long.class, pc_type),
+                getExecutableData(searchMethod(mm, "isLoaded0", long.class, long.class, pc_type)));
         registerNativeMethod(searchMethod(tm, "force0", FileDescriptor.class, long.class, long.class),
                 getExecutableData(searchMethod(mm, "force0", FileDescriptor.class, long.class, long.class)));
     }
