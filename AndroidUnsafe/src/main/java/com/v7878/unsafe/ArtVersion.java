3e73f9bd59327eb93c03679025cdd01c318d042c
v7878 <46866705+vova7878@users.noreply.github.com>
2025-01-12 20:02:00 +0700

    AndroidUnsafe: fix ArtVersion for android 16

--- AndroidUnsafe/src/main/java/com/v7878/unsafe/ArtVersion.java
+++ AndroidUnsafe/src/main/java/com/v7878/unsafe/ArtVersion.java
@@ -17,11 +17,15 @@ public class ArtVersion {
     @ApiSensitive
     public static final int ART_SDK_INT = computeSDKInt();
 
-    private static boolean atLeast36() {
-        return false; //FIXME!
+    private static boolean is36() {
+        Class<?> vmdebug = nothrows_run(() -> Class.forName(
+                "dalvik.system.VMDebug"));
+        Method offsets = searchMethod(getDeclaredMethods(vmdebug),
+                "getExecutableMethodFileOffsets", false, Method.class);
+        return offsets != null;
     }
 
-    private static boolean atLeast35() {
+    private static boolean is35() {
         Class<?> buf = nothrows_run(() -> Class.forName(
                 "java.nio.ByteBufferAsIntBuffer"));
         Field offset = searchField(getDeclaredFields(buf),
@@ -29,13 +33,13 @@ public class ArtVersion {
         return offset != null;
     }
 
-    private static boolean atLeast34() {
+    private static boolean is34() {
         Method sealed = searchMethod(getDeclaredMethods(Class.class),
                 "isSealed", false);
         return sealed != null;
     }
 
-    private static boolean atLeast33() {
+    private static boolean is33() {
         Class<?> esf = nothrows_run(() -> Class.forName(
                 "dalvik.system.EmulatedStackFrame"));
         Method invoke = searchMethod(getDeclaredMethods(MethodHandle.class),
@@ -57,19 +61,19 @@ public class ArtVersion {
         // At the moment, there is nothing above 36
         if (tmp == 36) return tmp;
 
-        if (atLeast36()) return 36;
+        if (is36()) return 36;
         // Not 36, but at least 35 -> 35
         if (tmp == 35) return 35;
 
-        if (atLeast35()) return 35;
+        if (is35()) return 35;
         // Not 35, but at least 34 -> 34
         if (tmp == 34) return 34;
 
-        if (atLeast34()) return 34;
+        if (is34()) return 34;
         // Not 34, but at least 33 -> 33
         if (tmp == 33) return 33;
 
-        if (atLeast33()) return 33;
+        if (is33()) return 33;
         // Not 33, but at least 31 (32 does not exist) -> 31
         return tmp;
     }
