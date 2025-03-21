7d674a20325ddc067b33c2fa0b0ee962586926b3
v7878 <46866705+vova7878@users.noreply.github.com>
2025-03-21 22:35:36 +0700

    AndroidUnsafe: refactor NativeCodeBlob

--- AndroidUnsafe/src/main/java/com/v7878/unsafe/NativeCodeBlob.java
+++ AndroidUnsafe/src/main/java/com/v7878/unsafe/NativeCodeBlob.java
@@ -17,36 +17,30 @@ import java.util.Arrays;
 import java.util.Objects;
 
 public class NativeCodeBlob {
-
     private static final int PROT_RW = OsConstants.PROT_READ | OsConstants.PROT_WRITE;
     private static final int PROT_RX = OsConstants.PROT_READ | OsConstants.PROT_EXEC;
-    private static final int CODE_FLAGS = OsConstants.MAP_SHARED;
+    private static final int CODE_FLAGS = OsConstants.MAP_PRIVATE | IOUtils.MAP_ANONYMOUS;
     private static final int CODE_ALIGNMENT = CURRENT_INSTRUCTION_SET.codeAlignment();
 
-    private static MemorySegment[] makeCodeBlobInternal(Arena arena, MemorySegment... code) {
+    private static MemorySegment[] makeCodeBlobInternal(Arena arena, MemorySegment... code) throws ErrnoException {
         int count = code.length;
         long size = 0;
-        long[] offsets = new long[count];
+        long[] offsets = new long[count], sizes = new long[count];
         for (int i = 0; i < count; i++) {
             size = roundUpUL(size, CODE_ALIGNMENT);
             offsets[i] = size;
-            size += code[i].byteSize();
+            size += sizes[i] = code[i].byteSize();
         }
         roundUpUL(size, PAGE_SIZE);
 
-        MemorySegment data;
-        try (var fd = IOUtils.ashmem_create_scoped_region("(runtime code)", size)) {
-            data = IOUtils.mmap(0, fd.value(), 0, size, PROT_RW, CODE_FLAGS, arena);
-        } catch (ErrnoException e) {
-            throw shouldNotHappen(e);
-        }
+        MemorySegment data = IOUtils.mmap(0, null, 0, size, PROT_RW, CODE_FLAGS, arena);
         long data_address = data.nativeAddress();
 
         MemorySegment[] out = new MemorySegment[count];
         if (ExtraMemoryAccess.isEarlyNativeInitialized()) {
             for (int i = 0; i < count; i++) {
-                MemorySegment tmp = data.asSlice(offsets[i], code[i].byteSize());
-                MemorySegment.copy(code[i], 0, tmp, 0, code[i].byteSize());
+                MemorySegment tmp = data.asSlice(offsets[i], sizes[i]);
+                MemorySegment.copy(code[i], 0, tmp, 0, sizes[i]);
                 out[i] = tmp;
             }
         } else {
@@ -54,16 +48,22 @@ public class NativeCodeBlob {
                 Object code_base = JavaForeignAccess.unsafeGetBase(code[i]);
                 long code_offset = JavaForeignAccess.unsafeGetOffset(code[i]);
                 AndroidUnsafe.copyMemory(code_base, code_offset, null,
-                        data_address + offsets[i], code[i].byteSize());
-                out[i] = data.asSlice(offsets[i], code[i].byteSize());
+                        data_address + offsets[i], sizes[i]);
+                out[i] = data.asSlice(offsets[i], sizes[i]);
             }
         }
+
+        IOUtils.mprotect(data_address, size, PROT_RX);
+
+        return out;
+    }
+
+    private static MemorySegment[] makeCodeBlobChecked(Arena arena, MemorySegment... code) {
         try {
-            IOUtils.mprotect(data_address, size, PROT_RX);
+            return makeCodeBlobInternal(arena, code);
         } catch (ErrnoException e) {
             throw shouldNotHappen(e);
         }
-        return out;
     }
 
     public static MemorySegment[] makeCodeBlob(Arena arena, MemorySegment... code) {
@@ -71,7 +71,7 @@ public class NativeCodeBlob {
         if (code.length == 0) {
             return new MemorySegment[0];
         }
-        return makeCodeBlobInternal(arena, code.clone());
+        return makeCodeBlobChecked(arena, code.clone());
     }
 
     public static MemorySegment[] makeCodeBlob(Arena arena, byte[]... code) {
@@ -79,7 +79,7 @@ public class NativeCodeBlob {
         if (code.length == 0) {
             return new MemorySegment[0];
         }
-        return makeCodeBlobInternal(arena, Arrays.stream(code)
+        return makeCodeBlobChecked(arena, Arrays.stream(code)
                 .map(MemorySegment::ofArray).toArray(MemorySegment[]::new));
     }
 }
