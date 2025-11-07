5b02a76f9baf8d18753d1a90af07f723b26fa8f6
Vladimir Kozelkov <46866705+vova7878@users.noreply.github.com>
2025-11-08 00:23:52 +0700

    Update build files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,6 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven { url = uri("https://jitpack.io") }
     }
 }
 dependencyResolutionManagement {
@@ -11,7 +10,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
     }
 }
 rootProject.name = "PanamaPort"
