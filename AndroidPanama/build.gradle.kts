77fc65900ef543d773c6dec129dafd6f215a81dc
Vladimir Kozelkov <46866705+vova7878@users.noreply.github.com>
2025-11-09 13:11:13 +0700

    Set up publications to Maven Central

--- AndroidPanama/build.gradle.kts
+++ AndroidPanama/build.gradle.kts
@@ -7,7 +7,7 @@ android {
     namespace = "com.v7878.foreign"
 
     sourceSets {
-        getByName("main") {
+        named("main") {
             java.srcDir("src/openjdk/java")
         }
     }
@@ -18,6 +18,7 @@ android {
 
     publishing {
         singleVariant("release") {
+            withJavadocJar()
             withSourcesJar()
         }
     }
@@ -34,16 +35,49 @@ dependencies {
     implementation(libs.dexfile)
 }
 
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            groupId = project.group.toString()
-            artifactId = project.name
-            version = project.version.toString()
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = false)
+    signAllPublications()
 
-            afterEvaluate {
-                from(components["release"])
+    coordinates(
+        groupId = "io.github.vova7878.panama",
+        artifactId = "Core",
+        version = project.version.toString()
+    )
+
+    pom {
+        name.set("PanamaPort-Core")
+        description.set("Implementation of FFM API for Android 8.0+")
+        inceptionYear.set("2025")
+        url.set("https://github.com/vova7878/PanamaPort")
+
+        licenses {
+            license {
+                name.set("MIT AND GPL-2.0-with-classpath-exception")
+                //TODO: url.set("https://github.com/vova7878/PanamaPort#licensing")
+                distribution.set("repository")
+                comments.set(
+                    """
+                    This artifact is licensed under both MIT and GPL-2.0 with Classpath Exception concurrently.
+                    - MIT applies to all parts of this project except for the code in `Core/src/openjdk` folder.
+                    - GPL-2.0 with Classpath Exception applies to code derived from OpenJDK in `Core/src/openjdk`.
+                    """.trimIndent()
+                )
             }
+        }
+
+        developers {
+            developer {
+                id.set("vova7878")
+                name.set("Vladimir Kozelkov")
+                url.set("https://github.com/vova7878")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/vova7878/PanamaPort")
+            connection.set("scm:git:git://github.com/vova7878/PanamaPort.git")
+            developerConnection.set("scm:git:ssh://git@github.com/vova7878/PanamaPort.git")
         }
     }
 }
--- AndroidUnsafe/build.gradle.kts
+++ AndroidUnsafe/build.gradle.kts
@@ -38,16 +38,49 @@ dependencies {
     runtimeOnly(project(":LLVM"))
 }
 
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            groupId = project.group.toString()
-            artifactId = project.name
-            version = project.version.toString()
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = false)
+    signAllPublications()
 
-            afterEvaluate {
-                from(components["release"])
+    coordinates(
+        groupId = "io.github.vova7878.panama",
+        artifactId = "Unsafe",
+        version = project.version.toString()
+    )
+
+    pom {
+        name.set("PanamaPort-Unsafe")
+        description.set("Implementation of FFM API for Android 8.0+")
+        inceptionYear.set("2025")
+        url.set("https://github.com/vova7878/PanamaPort")
+
+        licenses {
+            license {
+                name.set("MIT AND GPL-2.0-with-classpath-exception")
+                //TODO: url.set("https://github.com/vova7878/PanamaPort#licensing")
+                distribution.set("repository")
+                comments.set(
+                    """
+                    This artifact is licensed under both MIT and GPL-2.0 with Classpath Exception concurrently.
+                    - MIT applies to all parts of this project except for the code in `Core/src/openjdk` folder.
+                    - GPL-2.0 with Classpath Exception applies to code derived from OpenJDK in `Core/src/openjdk`.
+                    """.trimIndent()
+                )
             }
+        }
+
+        developers {
+            developer {
+                id.set("vova7878")
+                name.set("Vladimir Kozelkov")
+                url.set("https://github.com/vova7878")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/vova7878/PanamaPort")
+            connection.set("scm:git:git://github.com/vova7878/PanamaPort.git")
+            developerConnection.set("scm:git:ssh://git@github.com/vova7878/PanamaPort.git")
         }
     }
 }
